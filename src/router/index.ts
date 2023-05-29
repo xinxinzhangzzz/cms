9ad2d19b62f704c16661dfb146cdb1d2694db38a
zhangxinxin <1285367184@qq.com>
2023-05-29 16:52:55 +0800

    feat: 添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,21 @@ import type { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
+    path: "/",
+    redirect: "/main"
+  },
+  {
     path: "/login",
     component: () => import("@/views/login/index.vue")
   },
   {
     path: "/main",
     component: () => import("@/views/main/index.vue")
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound', 
+    component: () => import("@/views/NotFound/404.vue"),
   }
 ]
 
---
+++ src/views/NotFound/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-result status="404" title="404" sub-title="404? 在暗示你疯狂星期四!.">
+    <template #extra>
+      <a-button type="primary" @click="backClick">回到首页</a-button>
+    </template>
+  </a-result>
+</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const backClick = () => router.replace('/main')
+</script>
+
+<style scoped lang="less"></style>
